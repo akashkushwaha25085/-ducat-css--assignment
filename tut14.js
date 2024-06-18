0e7aacb71d34a2ecd095b9be00a42a8386ace5b1
akashkushwaha25085 <142476870+akashkushwaha25085@users.noreply.github.com>
2024-06-18 15:47:27 +0530

    Add files via upload

---
+++ tut14.js
@@ -0,0 +1,16 @@
+// let name= "hdksuhshcksh"
+// console.log(name)
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(3,6))
+
+// let frndname ="      raunak      "
+// console.log(frndname.trim())
+
+let fr = " akash " + " raunak " + " henam "
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr)
